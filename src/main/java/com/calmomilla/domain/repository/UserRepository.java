10da80f62de9d48bcdc6091bdd2943a992f29ca8
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2024-04-08 21:36:20 -0300

    :sparkles: Criando classe UserRepository
    feat: classe UserRepository para gerenciamento dos dados do Usuario e consultas no banco de dados

---
+++ src/main/java/com/calmomilla/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.calmomilla.domain.repository;
+
+import com.calmomilla.domain.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario,String> {
+
+    UserDetails findByEmail(String login);
+
+}
