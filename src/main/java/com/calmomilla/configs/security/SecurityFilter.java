35f3daf90201ef8ba552d96632493b9afa106854
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2024-04-08 21:38:07 -0300

    :sparkles: Criando classes de segurança SecurityConfig, SecurityFilter e TokenService
    feat:  Criação de algumas classe para a segurança da aplicação e criação de token

---
+++ src/main/java/com/calmomilla/configs/security/SecurityFilter.java
@@ -0,0 +1,42 @@
+package com.calmomilla.configs.security;
+
+import com.calmomilla.domain.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recuperarToken(request);
+        if (token != null){
+            var email = tokenService.validarToken(token);
+            UserDetails userDetails = userRepository.findByEmail(email);
+
+            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String recuperarToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null ) return null;
+        return authHeader.replace("Bearer", "");
+    }
+
+}
---
+++ src/main/java/com/calmomilla/configs/security/TokenService.java
@@ -0,0 +1,51 @@
+package com.calmomilla.configs.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.calmomilla.domain.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gerarToken(Usuario user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("calmomilla-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(gerarExpiracao())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception){
+            throw  new RuntimeException("Erro enquanto gerava o token", exception);
+        }
+    }
+
+    public String validarToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("calmomilla-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return "";
+        }
+    }
+
+    private Instant gerarExpiracao(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
