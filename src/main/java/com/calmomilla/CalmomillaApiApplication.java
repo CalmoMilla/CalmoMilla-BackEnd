0f16ba33503a58d34c787063f3488e4d286b0797
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2024-04-08 21:26:52 -0300

    :tada: Configurações iniciais

---
+++ src/main/java/com/calmomilla/CalmomillaApiApplication.java
@@ -0,0 +1,13 @@
+package com.calmomilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmomillaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalmomillaApiApplication.class, args);
+	}
+
+}
