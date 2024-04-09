31612bac13dbbf66354473d3d46356a6dd662ec3
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2024-04-08 21:20:04 -0300

    Iniciando projeto usando Spring

---
+++ src/test/java/com/calmomilla/api/CalmomillaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calmomilla.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmomillaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
