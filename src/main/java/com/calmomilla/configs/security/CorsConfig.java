79075ad1bef8cbc2a4b8dd19e2adf04830469d97
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2024-04-10 17:55:01 -0300

    :sparkles: @APBielzinx Criando classe CorsConfig
    feat: Classe de configuração para politica de CORS

---
+++ src/main/java/com/calmomilla/configs/security/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.calmomilla.configs.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
