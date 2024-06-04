6b42ea0c84770bfc3d0d9a864c4516e6f356da8c
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2024-06-03 21:48:19 -0300

    Update CorsConfig.java

--- src/main/java/com/calmomilla/api/configs/security/CorsConfig.java
+++ src/main/java/com/calmomilla/api/configs/security/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://ec2-18-231-17-42.sa-east-1.compute.amazonaws.com:3000")
+                .allowedOrigins("http://localhost:3000","https://ec2-18-231-17-42.sa-east-1.compute.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
